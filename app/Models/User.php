f16d0a0b99a1fc1aa955f0ebc4ae976b6cfb7f4c
Abderrahmane <abdoelhadere3@gmail.com>
2025-12-23 16:33:57 +0100

    Create findByEmail() method

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,25 +4,35 @@ class User
 {
     private PDO $pdo;
 
-    public function __construct(PDO $pdo)
-    {
+    public function __construct(PDO $pdo){
+
         $this->pdo = $pdo;
     }
 
     
     // Récupérer un utilisateur par son ID
      
-    public function getById(int $id): array|null
-    {
+    public function getById(int $id): array|null {
+
         $stmt = $this->pdo->prepare(
-            "SELECT id, name, email FROM users WHERE id = :id"
-        );
-        $stmt->execute(['id' => $id]);
+            "SELECT id, name, email FROM users WHERE id = ?");
+
+        $stmt->execute([$id]);
 
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
         return $user ?: null;
     }
 
+    public function findByEmail(string $email): array|null{
 
+        $stmt = $this->pdo->prepare(
+            "SELECT id, name, email, password from users where email = ?");
+
+        $stmt->execute([$email]);
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $user ?: null;
+    }
 }
