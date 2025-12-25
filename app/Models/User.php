75fa9d01f0db6febf4f9b607c12653c0b80af013
Abderrahmane <abdoelhadere3@gmail.com>
2025-12-25 12:00:24 +0100

    Create() to insert new users

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,30 +9,36 @@ class User
         $this->pdo = $pdo;
     }
 
+    public function create(string $name, string $email, string $hashPassword): bool{
+
+        $stmt = $this->pdo->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+        return $stmt->execute([$name, $email, $hashPassword]);
+    }   
     
-    // Récupérer un utilisateur par son ID
+    // Recuperer utilisateur par son ID
      
     public function getById(int $id): array|null {
 
         $stmt = $this->pdo->prepare(
             "SELECT id, name, email FROM users WHERE id = ?");
-
+    
         $stmt->execute([$id]);
-
+    
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
+    
         return $user ?: null;
     }
 
-    public function findByEmail(string $email): array|null{
-
+    public function findByEmail(string $email): array|null {
+        $email = trim($email);
+       
         $stmt = $this->pdo->prepare(
-            "SELECT id, name, email, password from users where email = ?");
-
+            "SELECT id, name, email, password FROM users WHERE email = ? LIMIT 1");
         $stmt->execute([$email]);
 
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
+       
         return $user ?: null;
     }
+    
 }
---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host'    => 'localhost',
+        'name'    => 'smart_wallet2',
+        'user'    => 'wallet_user',
+        'pass'    => 'wallet_pass',        
+        'charset' => 'utf8mb4'
+    ]
+];
