e576b34290ff990784d6adf9b0fb6b6280414260
Abderrahmane <abdoelhadere3@gmail.com>
2025-12-26 22:25:57 +0100

    Create userId function to return userId

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,19 @@
 class User
 {
     private PDO $pdo;
+    private string $name;
+    private string $email;
+    private string $password;
+    private string $confirm;
 
-    public function __construct(PDO $pdo){
+    public function __construct(PDO $pdo, string $name, string $email, string $password, string $confirm)
+    {
 
+        $this->pdo = $pdo;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->confirm = $confirm;
         $this->pdo = $pdo;
     }
 
@@ -41,4 +51,74 @@ class User
         return $user ?: null;
     }
     
+    public function login(): bool {
+    
+        $user = $this->findByEmail($this->email);
+        if ($user && !password_verify($this->password, PASSWORD_DEFAULT))  {
+            
+            Session::set('userId', $user['id']);
+            Session::set('name', $user['name']);
+            return true;
+        }
+        return false;
+    }
+
+    public function register(): array {
+        $errors = [];
+
+        $name    = trim($this->name ?? '');
+        $email   = trim($this->email ?? '');
+        $pass    = $this->password ?? '';
+        $confirm = $this->confirm ?? '';
+
+        if ($name === '') {
+            $errors[] = "Le nom est requis";
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "Email non valide";
+        }
+
+        if (strlen($pass) < 8 || !preg_match('/[A-Za-z]/', $pass) || !preg_match('/\d/', $pass)) {
+            $errors[] = "Le mot de passe doit contenir au moins 8 caractères, des lettres et des chiffres.";
+        }
+
+        if ($pass !== $confirm) {
+            $errors[] = "Les mots de passe ne correspondent pas.";
+        }
+
+        if (!empty($errors)) {
+            return $errors;
+        }
+
+        // email exists ?
+        $exist = $this->findByEmail($email);
+        if ($exist) {
+            $errors[] = "Cet email existe déjà.";
+            return $errors;
+        }
+
+        // insert
+        $hashPassword = password_hash($pass, PASSWORD_DEFAULT);
+
+        $ok = $this->create($name, $email, $hashPassword);
+        if (!$ok) {
+            $errors[] = "Inscription impossible (erreur serveur).";
+        }
+
+        return $errors; 
+    }
+
+    public function check(): bool
+    {
+        return Session::get('userId') !== null;
+    }
+
+    public function userId(): ?int
+    {
+        $id = Session::get('userId');
+        return $id !== null ? (int)$id : null;
+    }
+
+
 }
