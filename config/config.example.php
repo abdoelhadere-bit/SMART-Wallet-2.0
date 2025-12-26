579a0128f397b23fd69b98e56e00dd911f8187d9
Abderrahmane <abdoelhadere3@gmail.com>
2025-12-26 22:27:32 +0100

    Create an example of config file

---
+++ config/config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+// rename this file to config.php.
+return [
+    'db' => [
+        'host'    => 'your host',
+        'name'    => 'your database name',
+        'user'    => 'your database user name (eg:root)',
+        'pass'    => 'secret',        
+        'charset' => 'utf8mb4'
+    ]
+];
